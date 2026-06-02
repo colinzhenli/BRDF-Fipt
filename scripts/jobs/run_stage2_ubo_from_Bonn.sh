284708fda6b5a8a0a86ea6d1527b1e91bbe76724
colinzhenli <heyzhen.li@gmail.com>
2026-06-02 06:03:26 +0000

    updates

--- scripts/jobs/run_stage2_ubo_from_Bonn.sh
+++ scripts/jobs/run_stage2_ubo_from_Bonn.sh
@@ -36,6 +36,6 @@ python main.py \
     model.freeze_decoder=True \
     model.apply_cosine_weight=True \
     model.trainer.max_epochs=60 \
-    model.trainer.check_val_every_n_epoch=4 \
+    model.trainer.check_val_every_n_epoch=2 \
     model.trainer.limit_train_batches=5838 \
     model.ckpt_path='/media/raid/cloth/output/BRDF/Stage-1-Finals/Bonn.ckpt'
